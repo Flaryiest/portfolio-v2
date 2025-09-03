0da877a12cc997d8aeaad7429b727b496a4e40b6
Flaryiest <ericmzuo1@gmail.com>
2025-09-02 19:36:41 -0600

    folder structure

---
+++ web/src/router.tsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
+import HomePage from "./pages/home/index";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<HomePage />} />
+  )
+);
+
+export default router;
